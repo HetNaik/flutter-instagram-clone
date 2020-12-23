8d906da4542d5597edd33cd0a184fcddbfea086f
Het Naik <46862465+HetNaik@users.noreply.github.com>
2020-12-23 09:44:57 +0530

    Added project files

---
+++ lib/constants.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+
+sbh(double l){
+  return SizedBox(height:l);
+}
+
+sbw(double w){
+  return SizedBox(width:w);
+}
+
+bld(){
+  return TextStyle(fontWeight:FontWeight.bold);
+}
+
+Color hRd=Colors.red;
+Color hOr=Colors.orange;
